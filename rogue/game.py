674f6766975e885a2aa37c680576ea846f1f2114
Nick Johnstone <ncwjohnstone@gmail.com>
2013-11-23 15:55:24 +1300

    Added Character Class to Rogue

--- rogue/game.py
+++ rogue/game.py
@@ -6,7 +6,17 @@ import scroll
 game = scroll.Scroll(resolution=(640, 480))
 
 
+class Character(scroll.Entity):
+    def __init__(self, position):
+        super().__init__()
+        self.sprite_path = r'C:\Projects\Scroll\rogue\sprites\test.png'
+        self.add_component(scroll.component.Position(position))
+        self.add_component(scroll.component.Sprite(self.sprite_path))
+
+
 def main():
+    char = Character(scroll.helpers.Vector2(100, 100))
+    char.draw()
     game.run()
 
 
