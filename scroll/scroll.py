a7afd9861ae831c01c955789f944695ab22088c2
Nick Johnstone <ncwjohnstone@gmail.com>
2014-04-28 19:26:48 +1200

    Added Floor and EmptyFloor

--- scroll/scroll.py
+++ scroll/scroll.py
@@ -10,3 +10,26 @@ class Game(object):
 
     def run(self):
         pyglet.app.run()
+
+
+class Floor(object):
+    """A floor of the dungeon"""
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.characters = []
+        self.terrain = [[None for i in range(width)] for j in range(height)]
+        self.room = [[None for i in range(width)] for j in range(height)]
+
+    def add_character(self, character, position):
+        character.position = position
+        self.characters.append(character)
+        self.room[position.x][position.y] = character
+
+    def generate(self):
+        pass
+
+
+class EmptyFloor(Floor):
+    def __init__(self, width, height):
+        super(EmptyFloor, self).__init__(width, height)
