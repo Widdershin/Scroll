d50eccf81be03865e396d6e2eae825910475b27c
Nick Johnstone <ncwjohnstone@gmail.com>
2014-04-29 23:49:32 +1200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Scroll',
+    version='0.1.0',
+    packages=['scroll'],
+    license='A framework for creating Roguelikes',
+    long_description=open('README.md').read(),
+)
